fb25fd6c0e2d78b5fa4f2522a9e26175239154f6
Keith A. Lewis <kal@kalx.net>
2020-12-15 18:20:51 -0500

    add edf

--- xll_variate.cpp
+++ xll_variate.cpp
@@ -1,4 +1,4 @@
-// xll_variate.cpp - random variates
+﻿// xll_variate.cpp - random variates
 #include "xll/xll/xll.h"
 #include "xll_variate.h"
 
@@ -14,7 +14,7 @@ static AddIn xai_variate_cdf(
 		Arg(XLL_WORD, "n", "is the derivative. Default is 0.")
 		})
 	.FunctionHelp("Return the n-th derivative of the transformed cumulative distribution function at x.")
-	.Category("Variate")
+	.Category(XLL_CATEGORY)
 	.Documentation(R"(
 If \(f\) is the density function of a random variable \(X\) then \(f_s(x) = f(x)\exp(s X - \kappa(s))\) is
 its <em>Esscher transform</em>.
@@ -44,8 +44,8 @@ static AddIn xai_variate_pdf(
 		Arg(XLL_DOUBLE, "x", "is the value."),
 		Arg(XLL_DOUBLE, "s", "is the Esscher transform parameter. Default is 0."),
 		})
-		.FunctionHelp("Return s transformed probability density at x.")
-	.Category("Variate")
+	.FunctionHelp("Return s transformed probability density at x.")
+	.Category(XLL_CATEGORY)
 );
 double WINAPI xll_variate_pdf(HANDLEX m, double x, double s)
 {
@@ -72,10 +72,10 @@ static AddIn xai_variate_cumulant(
 		Arg(XLL_WORD, "n", "is the derivative. Default is 0.")
 		})
 	.FunctionHelp("Return n-th derivative of cumulant at s.")
-	.Category("Variate")
+	.Category(XLL_CATEGORY)
 	.Documentation(R"(
-The <em>cumulant</em> of a random variable \(X)\ is
-\(\kappa(s) = \log(E[\exp(s X)])\).
+The <em>cumulant</em> of a random variable <math>X</math> is
+<math>κ(s) = log(E[e<sup>s X</sup>)])</math>.
 	)")
 );
 double WINAPI xll_variate_cumulant(HANDLEX m, double s, WORD n)
@@ -86,6 +86,33 @@ double WINAPI xll_variate_cumulant(HANDLEX m, double s, WORD n)
 	try {
 		if (m_) {
 			return m_->cumulant(s, n);
+		}
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return XLL_NAN;
+}
+
+static AddIn xai_variate_edf(
+	Function(XLL_DOUBLE, "xll_variate_edf", "VARIATE.EDF")
+	.Args({
+		Arg(XLL_HANDLE, "m", "is a handle to the variate."),
+		Arg(XLL_DOUBLE, "x", "is the value."),
+		Arg(XLL_DOUBLE, "s", "is the Esscher transform parameter. Default is 0."),
+		})
+		.FunctionHelp("Return s transformed probability density at x.")
+	.Category(XLL_CATEGORY)
+);
+double WINAPI xll_variate_edf(HANDLEX m, double x, double s)
+{
+#pragma XLLEXPORT
+	handle<variate_base<>> m_(m);
+
+	try {
+		if (m_) {
+			return m_->edf(x, s);
 		}
 	}
 	catch (const std::exception& ex) {
