ff059a506cae84d4bd9e0ce9b7b08d08d36e9977
Keith A. Lewis <kal@kalx.net>
2020-12-29 15:46:58 -0500

    fix iota indices

--- xll_array.cpp
+++ xll_array.cpp
@@ -17,25 +17,25 @@ _FPX* WINAPI xll_iota(double n)
 
 	if (n >= 1) {
 		a.resize(static_cast<int>(n), 1);
-		for (int i = 0; i < n; ++i) {
+		for (int i = 0; i < a.size(); ++i) {
 			a[i] = i;
 		}
 	}
 	else if (n > 0) {
 		a.resize(static_cast<int>(1 / n + 1), 1);
-		for (int i = 0; i <= a.size(); ++i) {
+		for (int i = 0; i < a.size(); ++i) {
 			a[i] = i * n;
 		}
 	}
 	else if (n > -1) {
 		a.resize(static_cast<int>(-1 / n + 1), 1);
-		for (int i = 0; i <= a.size(); ++i) {
+		for (int i = 0; i < a.size(); ++i) {
 			a[i] = 1 + i * n;
 		}
 	}
 	else {
 		a.resize(static_cast<int>(-n), 1);
-		for (int i = 0; i < n; ++i) {
+		for (int i = 0; i < a.size(); ++i) {
 			a[i] = -n - i;
 		}
 	}
